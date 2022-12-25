1e362c35a6e59a6145380bd8a1cd3ed95d138c6a
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2022-12-25 17:39:32 +0900

    add datatype.py

---
+++ datatype.py
@@ -0,0 +1,37 @@
+# int(정수)형
+a = 5
+b = -5
+c = 0
+print(type(a), type(b), type(c))
+
+# float(실수)형
+d = 5.5
+e = -5.5
+f = 0.0
+print(type(d), type(e), type(f))
+
+# 과학적 표기법
+g = 1.234567e5
+print(g)
+
+# complex(복소수)형
+# a + bj
+
+# 문자열
+text = "String \"Data\" Type"
+print(text)
+
+# 탈출문자
+# \', \", \\, \n, \r, \t
+text = "\t\'Java\' \"Python\" \
+\n\tis Easy"
+print(text)
+
+text = """python
+is
+Easy"""
+print(text)
+
+# bool형
+is_ture = True
+is_false = False
--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,8 @@
-# 주석
-
 # Hello World를 출력하는 코드입니다.
 print("Hello World")
+
+a = 10
+b = 20
+print(a, b, sep='!')
+print(a, b, end='\t')
+print("2번째 라인")
