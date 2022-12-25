8911977747151aaaa3ca4546fee131f8a5a76913
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2022-12-25 18:19:52 +0900

    add opratortype, update datatype

--- datatype.py
+++ datatype.py
@@ -35,3 +35,36 @@ print(text)
 # bool형
 is_ture = True
 is_false = False
+
+# 정수형으로 변환
+print(int(123.92))
+print(int(123.9123213))
+print(int(123.93435))
+print(int(True))
+print(int(False))
+print(type(int("284242")))
+
+# 실수형으로 변환
+print(float(200))
+print(float(3))
+print(float(True))
+print(float(False))
+print(float("23.234354"))
+print(float("23"))
+
+# 문자열로 변환
+print(type(str(12312)))
+print(type(str(12312.3434)))
+print(type(str(True)))
+print(type(str(False)))
+
+# bool형으로 변환
+# false
+print(bool(0))
+print(bool(0.0))
+print(bool(""))
+# true
+print(bool(1))
+print(bool(1.0))
+print(bool("str"))
+print(bool("1231"))
---
+++ operatortype.py
@@ -0,0 +1,45 @@
+print(10 + 2)
+print(10 - 2)
+print(10 * 2)
+print(10 / 2)
+
+print(type(10 / 10))
+
+print(10 // 2)
+print(10 % 3)
+
+# 비교 연산자
+a = 10
+b = 20
+print(a < b)
+print(a <= b)
+print(a > b)
+print(a >= b)
+print(a == b)
+print(a != b)
+
+is_true = True
+is_false = False
+print(is_true > is_false)  # True
+print(is_true < is_false)  # False
+
+print("Python" > "python")  # False
+print("Python" < "python")  # True
+print("12345" > "12344")  # False
+print("12.12" < "13.12")  # True
+
+# 복합 대입 연산자
+today = 1230
+today += 1
+print(today)
+
+# and
+print(True and True)
+print(True and False)
+
+# or
+print(True or False)
+print(False or False)
+
+# not
+print(not True)
