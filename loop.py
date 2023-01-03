f6823290f90265e643d9a3f53aa0126511db440f
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-01-03 23:04:48 +0900

    update loop.py

--- loop.py
+++ loop.py
@@ -10,7 +10,7 @@ else:
 
 # list while문
 fruits = ["사과", "키위", "바나나", "사과", "바나나", "망고"]
-fruit = input("빼낼 과일을 입력하세요 > ")
+fruit = "사과"
 while fruit in fruits:
     fruits.remove(fruit)
 print(fruits)
@@ -18,8 +18,8 @@ print("{}을 모두 제거했습니다.".format(fruit))
 
 
 # 최소값, 최대값 입력 받고, 그 사이의 숫자 중 짝수와 홀수 리스트 출력하기
-max = int(input("최대값 입력 > "))
-min = int(input("최소값 입력 > "))
+max = 10
+min = 1
 odd_list = []
 even_list = []
 num = min
@@ -47,3 +47,42 @@ print(numbers)
 # // 몫 연산 반환값 정수
 numbers = list(range(-10, 10//2))
 print(numbers)
+
+
+# for문
+for i in range(1, 10+1):
+    print(i)
+
+for i in "일이삼사오":
+    print(i)
+
+fruits = ["사과", "딸기", "바나나"]
+for i in fruits:
+    print("과일 바구니에 {}가 들어있습니다.".format(i))
+
+
+# 1부터 30까의 수 중에서 3의 배수들의 합을 구하시오.
+sum = 0
+for i in range(1, 30+1):
+    if (i % 3 == 0):
+        print("{} + {} = ".format(sum, i), end="")
+        sum += i
+print(sum)
+
+
+# 딕셔너리 for문 사용
+coffee = {"아메리카노": 2500, "라떼": 3000, "자바": 2500}
+for i in coffee.values():
+    print(i)
+
+
+# 리스트 인덱스와 요소 enumerate로 출력하기
+for i, j in enumerate(fruits):
+    print(f"{i+1}번째 과일은 {j}입니다.")
+
+
+# 중첩 for문
+list_2nd = [[1, 2, 3], ["a", "b", "c"]]
+for i in list_2nd:
+    for j in i:
+        print(j)
