0519135b748415acda100e0606f47060b1c3037f
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-01-07 13:39:34 +0900

    update func

--- func.py
+++ func.py
@@ -38,3 +38,99 @@ def game(text):
 
 
 game("함수호출")
+
+
+# 지역변수 (Local Variable)
+def jeju_potato():
+    potato = "고구마"
+    print(potato)
+
+
+jeju_potato()
+
+# 전역변수(Global Variable)
+potato = "감자"
+
+
+def jeju_potato_2():
+    global potato
+    print(potato)
+    potato = "고구마"
+    print(potato)
+
+
+jeju_potato_2()
+
+
+# 매개변수에 기본값 지정해주기
+# 매개변수 기본값을 맨 앞에 매개변수로 지정해준 경우
+# 함수를 호출할 때, 매개변수를 생략할 수 없게 된다.
+
+
+def add(num1, num2=20):
+    return num1 + num2
+
+
+print(add(10))
+
+
+a, b = 20, 40
+
+
+def add_2(num1=a, num2=b):
+    return num1 + num2
+
+
+print(add_2())
+
+
+# 가변 매개변수
+# 일반 매개변수 앞에 올 수 없다
+# add(*argc, num) (x)
+
+
+def add(*argc):
+    return argc
+
+
+# 튜플 형태로 반환된다.
+print(add(10, 20, 30, 40))
+
+
+# 키워드 매개변수
+# 키워드 = 특정값 형태로
+def star_player(**kwargs):
+    for i, j in kwargs.items():
+        if "서장훈" in kwargs.values():
+            print("저는 LG팬이라 서장훈을 좋아했어요")
+        else:
+            print("{}는 역시 {}지".format(i, j))
+
+
+star_player(농구="서장훈")
+
+
+# 모든 종류 매개변수 사용
+def func_a(name, *argc, address="한국", **kwargs):
+    print(name, argc, address, kwargs)
+
+
+func_a("홍길동", "옛날", "사람", address="한양", 직업="도둑")
+
+
+# 숫자 나열에서 최대값, 최소값 추출하기
+# def max_min_search(*numbers):
+#     max = numbers[0]
+#     min = numbers[0]
+#     for number in numbers:
+#         if max < number:
+#             max = number
+#         if min > number:
+#             min = number
+#     return max, min
+
+def max_min_search(*numbers):
+    return max(numbers), min(numbers)
+
+
+print(max_min_search(15, 30, 4, 600, 7, 80, 32))
